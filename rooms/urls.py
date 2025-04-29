d42651e803214bc3ac28e7452bb02d4fbbcead8c
Kalil-Fazith <kalilfazith.s@mitrahsoft.com>
2025-04-29 16:18:54 +0530

    committed successfully

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('Api/rooms', views.list_room),
-	path('Api/add_room', views.add_room),
-	path('Api/update_room/<int:id>', views.update_room),
-	path('Api/delete_room/<int:pk>',views.delete_room)
+	path('rooms', views.list_room),
+	path('add_room', views.add_room),
+	path('update_room/<int:id>', views.update_room),
+	path('delete_room/<int:pk>',views.delete_room)
 ]
