62b825555cd7d584cf026e36f663a18be5872ae9
Kalil-Fazith <kalilfazith.s@mitrahsoft.com>
2025-04-29 18:50:35 +0530

    committed

--- room_type/urls.py
+++ room_type/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('room_type', views.room_type_list), 
+	path('room_type', views.list_room_type), 
 	path('add_room_type', views.add_room_type),
 	path('update_room_type/<int:pk>', views.update_room_type),
 	path('delete_room_type/<int:pk>', views.delete_room_type)
