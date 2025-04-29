b891306f87bd5456f3ef45da0e074d46502fe692
vigneshMitrahsoft <165030555+vigneshMitrahsoft@users.noreply.github.com>
2025-04-29 19:25:00 +0530

    Revert "committed"

--- room_type/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class RoomTypeConfig(AppConfig):
-	default_auto_field = 'django.db.models.BigAutoField'
-	name = 'room_type'
--- room_type/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('room_type', views.list_room_type), 
-	path('add_room_type', views.add_room_type),
-	path('update_room_type/<int:pk>', views.update_room_type),
-	path('delete_room_type/<int:pk>', views.delete_room_type)
-]
