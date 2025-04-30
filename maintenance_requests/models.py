0a54dc37ce8f87f27d800b0d95eaecb886571ab1
Kalil-Fazith <kalilfazith.s@mitrahsoft.com>
2025-04-30 13:20:22 +0530

    successfully committed

--- maintenance_requests/models.py
+++ maintenance_requests/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
-class maintenance_request(models.Model):
+class maintenance_requests(models.Model):
 	guest_id = models.IntegerField(primary_key = True)
 	request_for = models.CharField(max_length=20)
 	status = models.CharField(max_length = 100)
 	requested_on = models.DateField()
 	resolved_on = models.DateField()
-	resolved_by = models.IntegerField()
+	resolved_by = models.IntegerField(null = True)
 	created_at = models.DateTimeField(auto_now_add = True, null = True)
 	updated_at = models.DateTimeField(auto_now = True, null = True)
-	updated_by = models.CharField(max_length=100, null = True)
-	created_by = models.CharField(max_length=100, null = True)
+	updated_by = models.CharField(max_length = 100, null = True)
+	created_by = models.CharField(max_length = 100, null = True)
 	
 	class Meta:
-		db_table = 'maintenance_request'
+		db_table = 'maintenance_requests'
+ 
--- maintenance_requests/serializer.py
+++ maintenance_requests/serializer.py
@@ -2,9 +2,9 @@ from rest_framework import serializers
 from .models import maintenance_requests
 
 class requestsSerializer(serializers.Serializer):
-	user_id = serializers.IntegerField()
-	request_for = serializers.CharField(max_length=20)
-	status = serializers.CharField(max_length = 100)
-	requested_on = serializers.DateField()
-	resolved_on = serializers.DateField()
-	resolved_by = serializers.IntegerField()
+    guest_id = serializers.IntegerField()
+    request_for = serializers.CharField(max_length=20)
+    status = serializers.CharField(max_length = 100)
+    requested_on = serializers.DateField()
+    resolved_on = serializers.DateField()
+    resolved_by = serializers.IntegerField()
