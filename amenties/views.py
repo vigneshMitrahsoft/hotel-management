7aa4703fbb4cbbcb1e545715dfef4663462700f7
pandian1402karthik <karthikpandian.m@mitrahsoft.com>
2025-04-30 12:39:43 +0530

    recently committed

--- amenties/views.py
+++ amenties/views.py
@@ -5,14 +5,13 @@ from .models import amenities
 from .serializer import  AmenitySerializer
 from rest_framework.exceptions import APIException
 
-
 @api_view(['POST'])
 def create_amenity(request):
     serializer = AmenitySerializer(data = request.data)
     if serializer.is_valid():
         amenities.objects.create(
-            amenities_id = serializer.validated_data['amenities_id'], 
-            amenities_name = serializer.validated_data['amenities_name '],
+            amenities_name = serializer.validated_data['amenities_name'],
+            description = serializer.validated_data['description'],
             created_by = 2,
             updated_by = 3
         )
@@ -21,31 +20,31 @@ def create_amenity(request):
 
 @api_view(['GET'])
 def list_amenties(request):
-	Amentites = amenities.objects.all()
-	serializer = AmenitySerializer(Amentites, many=True)
-	return Response(serializer.data)
-
+    Amentites = amenities.objects.all()
+    serializer = AmenitySerializer(Amentites, many = True)
+    print(serializer.data,"serializer_data")
+    return Response(serializer.data)
 
 def get_amenties(pk):
-	try:
-		Amenties = amenities.objects.get(amenities_id = pk)
-	except:
-		raise APIException("Amenties are not Available")
-	return Amenties
+    try:
+        Amenties = amenities.objects.get(amenities_id = pk)
+    except:
+        raise APIException("Amenties are not Available")
+    return Amenties
 
 @api_view(['PUT'])
 def update_amenity(request, pk):
-    amenities = get_amenties(pk = pk)
+    amenties = get_amenties(pk = pk)
     serializer = AmenitySerializer(data = request.data)
     if serializer.is_valid():
-        amenities.amenities_name = serializer.validated_data['amenities_name ']
-        amenities.save()
+        amenties.amenities_name = serializer.validated_data['amenities_name']
+        amenties.save()
         return Response({'message': 'Amenity updated successfully'})
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['DELETE'])
 def delete_amenity(request, pk):
-    amenity = get_amenties(pk = pk)
-    amenity.delete()
+    amenties = get_amenties(pk = pk)
+    amenties.delete()
     return Response({'message': 'Amenity deleted successfully'})
 
--- main/settings.py
+++ main/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'users',
     'rooms',
     'role',
+    'amenties'
     
 ]
 
